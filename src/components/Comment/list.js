6b9389655a09b6d70f16edeb4eb1d83ba144d997
housne <housne@gmail.com>
2018-03-20 18:47:38 +0800

    update commnet style

--- src/components/Comment/list.js
+++ src/components/Comment/list.js
@@ -16,39 +16,39 @@ const EmptyList = styled.div`
 export default function Comments({ comments, replyId, slug, reply, form }) {
   return (
     <CommentList>
-      {!comments.length ? (
-        <EmptyList>No comments yet!</EmptyList>
-      ) : (
-        formatCommentsData(comments).map(comment => (
-          <Comment
-            comment={comment}
-            key={comment._id || comment.date}
-            replyId={replyId}
-            slug={slug}
-            reply={reply}
-            form={form}
-          />
-        ))
-      )}
+      {formatCommentsData(comments).map(comment => (
+        <Comment
+          comment={comment}
+          key={comment._id || comment.date}
+          replyId={replyId}
+          slug={slug}
+          reply={reply}
+          form={form}
+        />
+      ))}
     </CommentList>
   );
 }
 
 const CommentItem = styled.div`
-  display: flex;
   margin-bottom: 2rem;
+  background: #fff;
+  box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.04),
+    0 2px 2px 0 rgba(0, 0, 0, 0.04), 0 1px 5px 0 rgba(0, 0, 0, 0.04);
+  border-radius: 2px;
 `;
 
 const Avatar = styled.img`
-  height: 40px;
-  width: 40px;
+  height: 30px;
+  width: 30px;
   border-radius: 50%;
   background: #ddd;
+  margin-right: 1rem;
 `;
 
 const CommentContent = styled.div`
   flex: 1;
-  padding-left: 1rem;
+  padding: 1rem;
 `;
 
 const CommentText = styled.div`
@@ -70,54 +70,72 @@ const CommentDate = styled.time`
   color: #666;
   font-size: 0.8rem;
   padding-right: 0.5rem;
+  flex: 1;
 `;
 
 const ReplyBox = styled.div`
-  padding-top: 1rem;
+  padding: 1rem;
 `;
 
 const CommentHeader = styled.div`
-  padding: 0;
+  padding: 0 1rem;
   color: #999;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  border-bottom: solid 1px #eee;
+`;
+
+const CommentFooter = styled.div`
+  height: 40px;
+  padding: 0 1rem;
+  display: flex;
+  align-items: center;
+  border-top: solid 1px #eee;
+  background: rgba(236, 239, 241, 0.4);
 `;
 
 export function Comment({ comment, replyId, slug, reply, form }) {
   return (
     <CommentItem>
-      <Avatar
-        src={`https://www.gravatar.com/avatar/${comment.email}?s=40&d=mm`}
-        alt={comment.name}
-      />
+      <CommentHeader>
+        <Avatar
+          src={`https://www.gravatar.com/avatar/${comment.email}?s=30&d=mm`}
+          alt={comment.name}
+        />
+        <CommentAuthor comment={comment} />
+        {comment.parentNode && (
+          <span>
+            <span style={{ padding: "0 0.3rem" }}>reply to</span>
+            <CommentAuthor comment={comment.parentNode} />
+          </span>
+        )}
+      </CommentHeader>
       <CommentContent>
-        <CommentHeader>
-          <CommentAuthor comment={comment} />
-          {comment.parentNode && (
-            <span>
-              {" "}
-              REPLY TO <CommentAuthor comment={comment.parentNode} />
-            </span>
-          )}
-        </CommentHeader>
         <CommentText>{comment.message}</CommentText>
-        <div>
-          <CommentDate>
-            {dateFormat(new Date(comment.date * 1000), "yyyy-mm-dd HH:MM")}
-          </CommentDate>
-          {comment._id && (
-            <ReplyButton onClick={() => reply(comment._id)}>
-              {replyId === comment._id ? "CANCEL" : "REPLY"}
-            </ReplyButton>
-          )}
-        </div>
-        {comment._id &&
-          replyId === comment._id && <ReplyBox>{form(replyId)}</ReplyBox>}
       </CommentContent>
+      <CommentFooter>
+        <CommentDate>
+          {dateFormat(new Date(comment.date * 1000), "yyyy-mm-dd HH:MM")}
+        </CommentDate>
+        {comment._id && (
+          <ReplyButton onClick={() => reply(comment._id)}>
+            {replyId === comment._id ? "CANCEL" : "REPLY"}
+          </ReplyButton>
+        )}
+      </CommentFooter>
+      {comment._id &&
+        replyId === comment._id && <ReplyBox>{form(replyId)}</ReplyBox>}
     </CommentItem>
   );
 }
 
 const Author = styled.strong`
   color: #333;
+  font-weight: 400;
+  strong {
+    font-weight: 400;
+  }
 `;
 
 const AuthorLink = Author.withComponent("a");
